24ff98739939914f8e2c4586850f36379445d472
AaronWrobel001 <aaronwrobel001@gmail.com>
2025-11-05 18:55:37 +0100

    ajout de la BDD (seuil de calibration)+ score

--- app.py
+++ app.py
@@ -4,28 +4,71 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 import asyncio
+from contextlib import asynccontextmanager
 import sqlite3
+import score as score
+
+@asynccontextmanager # gestion du cycle de vie de l'application (onstartup/shutdown)
+async def lifespan(app : FastAPI):
+    # Code à exécuter au démarrage de l'application
+    # Initialisation de la base de données SQLite
+    connect = sqlite3.connect('singonlight.db')
+    connect.execute('CREATE TABLE IF NOT EXISTS calibration (id INTEGER PRIMARY KEY AUTOINCREMENT, seuil INTEGER)') # utilisé afin d'obtenir le seuil de calibration
+    connect.execute('CREATE TABLE IF NOT EXISTS scores (id INTEGER, score INTEGER, maxScore INTEGER)') # utilisé afin d'obtenir les scores des parties jouées
+    everything = connect.execute('SELECT * FROM calibration')
+    data = everything.fetchall()
+    if len(data) == 0:
+        connect.execute('INSERT INTO calibration (id,seuil) VALUES (?,?)', (1,50)) # valeur par défaut du seuil de calibration
+    connect.commit()
+    connect.close()
+    print("Base de données initialisée.")
+    yield
+    # Code à exécuter à l'arrêt de l'application
+    pass
+
+#print(score.calculer([1,0,1,1,0,0,1],[0,1,1,1,0,0,1])) # Test de la fonction de calcul du score
 
 templates = Jinja2Templates(directory="templates/")
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 app.mount("/static", StaticFiles(directory="static"), name="static")
-@app.get('/index.html')
-def test(request:Request):
+@app.get('/')
+def main(request:Request):
     #user = {'username': 'Cécile'}
     return templates.TemplateResponse("index.html",{"request":request})
 
 @app.get("/play.html")
-def hello(request:Request) -> str:
+def play(request:Request) -> str:
     return templates.TemplateResponse('play.html',{'request': request})
 
 @app.get("/data.html")
-def hello(request:Request) -> str:
+def data(request:Request) -> str:
     return templates.TemplateResponse('data.html',{'request': request})
 
-@app.get("/about")
-def hello(request:Request) -> str: 
-    user = {'username': 'Cécile'}
-    return templates.TemplateResponse('hello.html',{'request': request,'title':'First page', 'user':user})
+@app.get("/calibration.html")
+def calibration(request:Request) -> str:
+    connect = sqlite3.connect('singonlight.db')
+    seuil = connect.execute('SELECT seuil FROM calibration').fetchone()[0]
+    connect.close()
+    return templates.TemplateResponse('calibration.html',{'request': request, 'seuil':int(seuil)})
+
+def save_calibration(seuil: int):
+    connect = sqlite3.connect('singonlight.db')
+    connect.execute('UPDATE calibration set seuil=(?) WHERE id=1', (seuil,))
+    connect.commit()
+    connect.close()
+
+
+@app.post("/run-calibrate") # récupération du seuil de calibration depuis la page calibration.html afin de le sauvegarder dans la base de donnée
+async def run_calibrate(request:Request):
+    body = await request.json()
+    seuil = body.get("seuil", 50)
+    result = save_calibration(int(seuil))
+    return result
 
 if __name__ == "__main__":
     uvicorn.run(app) # lancement du serveur HTTP + WSGI avec les options de debug
+
+
+def transformer_signal_audio(signal_audio, seuil):
+    """Transforme un signal audio en une liste binaire en fonction d'un seuil donné."""
+    pass  # Implémentation à venir
---
+++ score.py
@@ -0,0 +1,25 @@
+
+    
+def calculer(reference:list[int], essai:list[int], score:int=0)->int:
+    """IN: 2 listes de 0 ou 1 (0 = silence, 1 = bruit)
+       OUT: un score
+    """
+    assert len(reference) <= len(essai)
+    if len(reference) == len(essai) == 0:
+        return score
+    if reference[0] == 1:
+        if essai[0] == 1:
+            score += 10
+            return calculer(reference[1:], essai[1:], score)
+        if len(essai) > 1:
+            if essai[1] == 1:
+                score += 5
+                essai = [0] + essai[2:]
+                if reference[1] == 1:
+                    essai[0] = 1
+                return calculer(reference[1:], essai, score)
+    if reference[0] == 0:
+        if essai[0] == 1:
+            score -= 10
+            return calculer(reference[1:], essai[1:], score)
+    return calculer(reference[1:], essai[1:], score)

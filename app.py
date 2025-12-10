e61095aa85e59b1a19727d9f2cc0e95d8a43aebb
Bigouz <louneshanane@gmail.com>
2025-12-10 10:39:21 +0100

    mode de lancement avec le rythme généré
    lorque le rythme a ete créé manuellement c'est cette fonction qui permet de l'utilisé

--- app.py
+++ app.py
@@ -229,6 +229,43 @@ async def run_play(request:Request):
     w = reset_winstreak()
     return {"message": "Vous avez perdu avec un score de " + str(pourcentage) + "%", "winstreak": w}
 
+@app.post("/run_play")
+async def run_play_rythme(request:Request):
+""" appelé quand le joueur appuie sur le bouton jouer avec ton rythme"""
+
+connect = sqlite3.connect('singonlight.db')
+body = await request.json()
+dureeIntervalle = body.get("dureeIntervalle",1)
+dureePartie = body.get("dureePartie",25)
+save_param_jouer(dureeIntervalle, dureePartie)
+rythme = connect.execute("SELECT value FROM parametres WHERE cle='rythme';").fetchone()[0]
+if rythme == -1:
+    return("Tu n'as pas crée de rythme")
+else:
+    global start_event
+    start_event = asyncio.Event()
+    sound_task = asyncio.create_task(Sound.main(start_event,rythme))
+    start_event.set()
+
+    res = await sound_task
+
+    print(res)
+    print("fin de partie")
+    print("son (" + str(len(res))+"): " + str(res))
+    print("led (" + str(len(rythme))+"): " + str(rythme))
+        
+    signal = transformation_signal_moyenne(res,dureeIntervalle)
+    print(res, "=>", signal)
+    pourcentage = score.calculerPourcentage(rythme, signal)
+    enregistrer_score(pourcentage)
+    print(str(pourcentage) + "%")
+
+    if pourcentage >= 50:
+        w = increment_winstreak()
+        return {"message":"Vous avez gagné avec un score de " + str(pourcentage) + "%", "winstreak": w}
+    w = reset_winstreak()
+    return {"message": "Vous avez perdu avec un score de " + str(pourcentage) + "%", "winstreak": w}
+
 def reset_winstreak():
     connect = sqlite3.connect('singonlight.db')
     connect.execute('UPDATE parametres set valeur=0 WHERE cle="winstreak";')

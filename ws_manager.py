c4699cf8c0fba591961112397fdebdeb6ad1754c
AaronWrobel001 <aaronwrobel001@gmail.com>
2025-12-18 15:09:16 +0100

    Docstring ws_manager.py
    Docstring ws_manager.py

--- ws_manager.py
+++ ws_manager.py
@@ -4,6 +4,7 @@ from fastapi import WebSocket
 active_connections: List[WebSocket] = []
 
 async def broadcast(message: str):
+    """ envoie un message a toutes les machines connectées au websocket de la page calibration """
     for ws in active_connections:
         try:
             await ws.send_text(message)
